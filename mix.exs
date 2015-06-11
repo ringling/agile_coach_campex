659281268ad319088c7b1ae6b0c93cf43dea24e1
Thomas Ringling <thomas.ringling@gmail.com>
2015-06-11 16:44:44 +0200

    Add mailman & dotenv dependencies

--- mix.exs
+++ mix.exs
@@ -17,9 +17,20 @@ defmodule AgileCoachCampex.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {AgileCoachCampex, []},
-     applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+     applications: app_list(Mix.env)]
   end
+
+  defp app_list(:dev), do: [:dotenv | app_list]
+  defp app_list(_), do: app_list
+  defp app_list, do: [
+    :phoenix, 
+    :phoenix_html, 
+    :cowboy, 
+    :logger,
+    :phoenix_ecto, 
+    :postgrex
+  ]
+
 
   # Specifies which paths to compile per environment
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
@@ -32,8 +43,12 @@ defmodule AgileCoachCampex.Mixfile do
     [{:phoenix, "~> 0.13.1"},
      {:phoenix_ecto, "~> 0.4"},
      {:postgrex, ">= 0.0.0"},
+     {:mailman, "~> 0.1.1"},
+     {:dotenv, "~> 1.0.0"},
      {:phoenix_html, "~> 1.0"},
      {:phoenix_live_reload, "~> 0.4", only: :dev},
      {:cowboy, "~> 1.0"}]
   end
+
+
 end
