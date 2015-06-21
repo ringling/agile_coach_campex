ec7e18284d8dae0a5d11b16d9bb1cd380f7357a9
Thomas Ringling <thomas.ringling@gmail.com>
2015-06-21 14:54:19 +0200

    Semaphore CI

--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,7 @@ config :logger, level: :warn
 # Configure your database
 config :agile_coach_campex, AgileCoachCampex.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "elixir",
-  password: "elixir",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "elixir",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "elixir",
   database: "agile_coach_campex_test",
   size: 1 # Use a single connection for transactional tests
