0409578886dee4c05a0b98dac2c04d14d912aa24
Thomas Ringling <thomas.ringling@gmail.com>
2015-06-22 11:52:39 +0200

    Add Authenticator stub for test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -4,3 +4,48 @@ ExUnit.start
 Mix.Task.run "ecto.create", ["--quiet"]
 Mix.Task.run "ecto.migrate", ["--quiet"]
 Ecto.Adapters.SQL.begin_test_transaction(AgileCoachCampex.Repo)
+
+
+defmodule Plug.ProcessStore do
+  @behaviour Plug.Session.Store
+
+  def init(_opts) do
+    nil
+  end
+
+  def get(_conn, sid, nil) do
+    {sid, Process.get({:session, sid}) || %{}}
+  end
+
+  def delete(_conn, sid, nil) do
+    Process.delete({:session, sid})
+    :ok
+  end
+
+  def put(conn, nil, data, nil) do
+    sid = :crypto.strong_rand_bytes(96) |> Base.encode64
+    put(conn, sid, data, nil)
+  end
+
+  def put(_conn, sid, data, nil) do
+    Process.put({:session, sid}, data)
+    sid
+  end
+end
+
+
+defmodule Authenticator do
+  import Plug.Conn, only: [halt: 1, get_session: 2]
+  import Phoenix.Controller, only: [redirect: 2]
+  import AgileCoachCampex.Router.Helpers
+
+  def init(options), do: options
+
+  def call(conn, options) do
+    authenticate(conn, options)
+  end
+
+  defp authenticate(conn, options) do
+    conn
+  end
+end
