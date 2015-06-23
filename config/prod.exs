94d660214ce27d4a755551e44cb2a6d2720510ee
Thomas Ringling <thomas.ringling@gmail.com>
2015-06-23 16:02:25 +0200

    Add accdk.org url in config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :agile_coach_campex, AgileCoachCampex.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"],
+  url: [host: "accdk.org"],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
